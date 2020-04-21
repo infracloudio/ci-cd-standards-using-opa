717220da150850b7360dbd716cbae5837ad433b1
ameydev <ameydev2@gmail.com>
2020-04-22 00:39:20 +0530

    Added github actions for build and test fix for docker image failed

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,14 @@
 
 set -e
 
-
 if [[ $MANIFESTS_PATH_PATTERN == "*.yaml" ]] || [[ $MANIFESTS_PATH_PATTERN == "*.yml" ]]
 then
-    result=`opa eval -d "${LIBRARY_PATH}" -i $YAML_FILE "$DATA" --format=pretty`
+    result=`opa eval -d "${LIBRARY_PATH}" -i $MANIFESTS_PATH_PATTERN "$DATA" --format=pretty`
     if [ "$result" != "[]" ]; then
-       echo "One or more policies are violated in $MANIFESTS_PATH_PATTERN. $result"
+      echo "OPA Policy FAILED::"$DATA":: $MANIFESTS_PATH_PATTERN. $result"
       exit 1
+    else
+      echo "OPA Policy PASSED::"$DATA":: $MANIFESTS_PATH_PATTERN."
     fi
 else
     FILES=`find "${MANIFESTS_PATH_PATTERN}" -type f \( -name "*.yml" -o -name "*.yaml" \)`
@@ -16,9 +17,10 @@ else
       do
          result=`opa eval -d "${LIBRARY_PATH}" -i $YAML_FILE data.kubernetes.admission.deny --format=pretty`
          if [ "$result" != "[]" ]; then
-            echo "One or more policies are violated in $YAML_FILE. $result"
+            echo "OPA Policy FAILED::"$DATA":: $YAML_FILE. $result"
             exit 1
+         else
+            echo "OPA Policy PASSED::"$DATA":: $YAML_FILE."
          fi
       done
 fi
-
